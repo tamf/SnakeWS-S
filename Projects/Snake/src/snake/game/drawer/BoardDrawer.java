b836c12fbef35763d48d2e3199749f959394bc34
vcchang <vcchang@users.noreply.github.com>
2016-07-01 21:04:17 -0700

    Imported SnakeGame class, created grid constants, initialized grid objects, and created for loops to draw lines
    - Imported the SnakeGame class
    
    - Created constant fields for the white gridline-colour (GL_COLOUR) and
    the starting-x-point (STARTING_X)
    
    - Created and initialized the following objects containing the
    window-related SnakeGame constants:
    -- headerHeight
    -- cellSize
    -- windowWidth
    -- windowHeight
    -- numberOfCells
    
    - Created a for loop to draw the horizontal gridlines and a for loop to
    draw the vertical gridlines
    -- Gridlines are spaced out evenly with one cell between each line

--- Projects/Snake/src/snake/game/drawer/BoardDrawer.java
+++ Projects/Snake/src/snake/game/drawer/BoardDrawer.java
@@ -5,12 +5,22 @@ import simpleio.common.Direction;
 import simpleio.common.Point;
 import simpleio.common.Position;
 import simpleio.display.Window;
+import snake.game.SnakeGame;
 import snake.game.state.Board;
 
 public class BoardDrawer {
+	
+	// Constants
+	private static final Colour GL_COLOUR = Colour.WHITE;
+	private static final int STARTING_X = 0;
+	
 	private Window window;
-	private Point topLeft;
-	private int cellSize;
+	private int headerHeight = SnakeGame.HEADER_HEIGHT;
+	private int cellSize = SnakeGame.CELL_SIZE;
+	private int windowWidth = SnakeGame.WINDOW_WIDTH;
+	private int windowHeight = SnakeGame.WINDOW_HEIGHT;
+	private int numberOfCells = SnakeGame.NUMBER_OF_CELLS;
+	private Point topLeft = new Point(STARTING_X, headerHeight);
 
 	public BoardDrawer(Window window, Point topLeft, int cellSize) {
 		this.window = window;
@@ -19,13 +29,20 @@ public class BoardDrawer {
 	}
 
 	public void drawBoard(Board board) {
+		// Horizontal gridlines
+		for (int i = 0; i < numberOfCells; i++) {
+			Window.drawLine(GL_COLOUR, topLeft.addY(cellSize * i), new Point(windowWidth, headerHeight + cellSize * i));
+		}
 
+		// Vertical gridlines
+		for (int i = 0; i < numberOfCells; i++) {
+			Window.drawLine(GL_COLOUR, topLeft.addX(cellSize * i), new Point(cellSize * i, windowHeight));
+		}
 	}
 
 	public void drawSquare(Colour colour, Position p) {
 
 	}
-	
 
 	public void drawTriangle(Colour colour, Direction direction, Position p) {
 
