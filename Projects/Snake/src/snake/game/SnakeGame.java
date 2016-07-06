dbe7abbbd81e8208119f0b62ffb1afb8ad221886
vcchang <vcchang@users.noreply.github.com>
2016-07-06 11:03:42 -0700

    Created constants for 3 SnakeGame variables
    - Created the following constant fields in SnakeGame class; note: this
    is an edit to improve Issue # 1:
    -- STARTING_DIRECTION
    -- STARTING_X_POSITION
    -- STARTING_Y_POSITION

--- Projects/Snake/src/snake/game/SnakeGame.java
+++ Projects/Snake/src/snake/game/SnakeGame.java
@@ -31,6 +31,9 @@ public class SnakeGame {
 	private static final int WINDOW_WIDTH = CELL_SIZE * NUMBER_OF_CELLS;
 	private static final int WINDOW_HEIGHT = CELL_SIZE * NUMBER_OF_CELLS + HEADER_HEIGHT;
 	private static final int SNAKE_STARTING_LENGTH = 2;
+	private static final int STARTING_X_POSITION = 1;
+	private static final int STARTING_Y_POSITION = 1;
+	private static final Direction STARTING_DIRECTION = Direction.right;
 
 	// IO
 	private Window window;
@@ -91,7 +94,7 @@ public class SnakeGame {
 	private void initializeState() {
 		fruit = new Fruit();
 		board = new Board(NUMBER_OF_CELLS, NUMBER_OF_CELLS);
-		snake = new Snake(Direction.right, new Position(1, 1), SNAKE_STARTING_LENGTH);
+		snake = new Snake(STARTING_DIRECTION, new Position(STARTING_X_POSITION, STARTING_Y_POSITION), SNAKE_STARTING_LENGTH);
 		gameInfo = new GameInfo("Snake game", FRAMES_PER_MOVE);
 	}
 
